deb236f5134a09286a7b11ac8e0749bbcf8279e5
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-20 04:22:21 -0300

    163 - Compilando SASS con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function tarea(callback) {
-    console.log("Mi primer tarea")
+const {src, dest} = require("gulp") // Llamo a las funciones de GULP
+const sass = require("gulp-sass")(require('sass')) // Esto lo que hace es usar gulp-sass, pero en realidad lo que hace es ir y buscar la dependencia SASS que es la que tiene todo el "conocimiento"
+
+function css(callback) {
+
+    src('src/scss/app.scss') // Importo/identifico la ruta del archivo SCSS a compilar, a su vez pongo un pipe.
+        .pipe(sass()) // Aplico SASS/compilación
+        .pipe(dest("build/css")) // Envío/guardo todo lo compilado en la ruta asignada.
+        
     callback()
 }
 
-exports.tarea = tarea
+exports.css = css; // Llamo la función con la sintaxis de Node exports.
