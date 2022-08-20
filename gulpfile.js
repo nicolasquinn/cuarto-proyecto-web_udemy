f7d7751e9bff1e4233a6cfbbf3877bd2d530429a
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-20 14:58:31 -0300

    170 - Añadiendo Plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-const {src, dest, watch} = require("gulp") // Llamo a las funciones de GULP
-const sass = require("gulp-sass")(require('sass')) // Esto lo que hace es usar gulp-sass, pero en realidad lo que hace es ir y buscar la dependencia SASS que es la que tiene todo el "conocimiento"
+const {src, dest, watch} = require("gulp"); // Llamo a las funciones de GULP
+const sass = require("gulp-sass")(require('sass')); // Esto lo que hace es usar gulp-sass, pero en realidad lo que hace es ir y buscar la dependencia SASS que es la que tiene todo el "conocimiento"
+const plumber = require("gulp-plumber");
 
 function css(callback) {
 
     src('src/scss/**/*.scss') // Importo/identifico la ruta del archivo SCSS a compilar, a su vez pongo un pipe.
+        .pipe(plumber())
         .pipe(sass()) // Aplico SASS/compilación
         .pipe(dest("build/css")) // Envío/guardo todo lo compilado en la ruta asignada.
         
-    callback()
+    callback();
 }
 
 function dev(callback) { // Creo la función Watch para compilar en "vivo"
-    watch('src/scss/**/*.scss', css) // Ejecuto la función indicando primero la ubicación del archivo y la función de compilación a ejecutar
+    watch('src/scss/**/*.scss', css); // Ejecuto la función indicando primero la ubicación del archivo y la función de compilación a ejecutar
 
-    callback()
+    callback();
 }
 
 
