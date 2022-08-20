69e65366c4f32ee6b36a4b761b071ae4409c4078
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-20 13:19:14 -0300

    164 - Añadiendo un watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest} = require("gulp") // Llamo a las funciones de GULP
+const {src, dest, watch} = require("gulp") // Llamo a las funciones de GULP
 const sass = require("gulp-sass")(require('sass')) // Esto lo que hace es usar gulp-sass, pero en realidad lo que hace es ir y buscar la dependencia SASS que es la que tiene todo el "conocimiento"
 
 function css(callback) {
@@ -10,4 +10,12 @@ function css(callback) {
     callback()
 }
 
+function dev(callback) { // Creo la función Watch para compilar en "vivo"
+    watch('src/scss/app.scss', css) // Ejecuto la función indicando primero la ubicación del archivo y la función de compilación a ejecutar
+
+    callback()
+}
+
+
 exports.css = css; // Llamo la función con la sintaxis de Node exports.
+exports.dev = dev;
