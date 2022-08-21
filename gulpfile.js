82fb142691f8451ce317fd0184a65c11f7032da6
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-21 15:37:02 -0300

    189 - Aligerar imágenes con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const {src, dest, watch, parallel} = require("gulp"); // Llamo a las funciones d
 
 // Compilación CSS
 const sass = require("gulp-sass")(require('sass')); // Esto lo que hace es usar gulp-sass, pero en realidad lo que hace es ir y buscar la dependencia SASS que es la que tiene todo el "conocimiento"
-const plumber = require("gulp-plumber");
+const plumber = require("gulp-plumber"); 
 
 // Imágenes 
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 
 function css(callback) {
@@ -17,7 +19,20 @@ function css(callback) {
     callback();
 }
 
+function imagenes(callback) {
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{jpg,png}')
+        .pipe (cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    callback();
+
+}
+
 function versionWebp(callback) {
+
     const opciones = {
         quality: 50
     }
@@ -25,16 +40,17 @@ function versionWebp(callback) {
         .pipe(webp(opciones))
         .pipe(dest('build/img'))
     callback();
+
 }
 
 function dev(callback) { // Creo la función Watch para compilar en "vivo"
-    watch('src/scss/**/*.scss', css); // Ejecuto la función indicando primero la ubicación del archivo y la función de compilación a ejecutar
 
+    watch('src/scss/**/*.scss', css); // Ejecuto la función indicando primero la ubicación del archivo y la función de compilación a ejecutar
     callback();
+
 }
 
-
-
 exports.css = css; // Llamo la función con la sintaxis de Node exports.
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp, dev);
